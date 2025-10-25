a046f764599a54942435a153a44b998b8350a985
varshapanda <varsha.vijayashwini@kalvium.community>
2025-10-26 01:40:58 +0530

    MongoDB database connected

---
+++ Backend/src/lib/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async()=>{
+    try{
+        const connect = await mongoose.connect(process.env.MONGODB_URL)
+        console.log("MongoDB connected successfully, ", connect.connection.host)
+    }
+    catch(err){
+        console.error("MongoDB connection failed, ", err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
