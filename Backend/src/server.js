bdb28c772cf97817481ae8e8369b2c655821f3e5
varshapanda <varsha.vijayashwini@kalvium.community>
2025-10-27 21:36:32 +0530

    Updated server configuration to load environment variables using dotenv and fallback to process.env.PORT for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 const cookieParser = require("cookie-parser");
 const path = require("path");
-const connectDB = require('./lib/db.js');
+const connectDB = require("./lib/db.js");
 const authRoutes = require("./routes/auth.route.js");
 const messageRoutes = require("./routes/message.route.js");
 const ENV = require("./lib/env.js");
@@ -9,22 +11,18 @@ const ENV = require("./lib/env.js");
 connectDB();
 const app = express();
 
-const PORT = ENV.PORT || 8080;
+const PORT = process.env.PORT || ENV.PORT || 8080;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes );
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
-  });
-} else {
-  app.get("/", (_, res) => {
-    res.status(200).json("Welcome to ConvoPoint Backend");
   });
 }
 
