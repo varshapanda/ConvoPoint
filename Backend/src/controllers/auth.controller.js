f1124dbb331bcc3c6b71fadcd93da0e479a35e85
varshapanda <varsha.vijayashwini@kalvium.community>
2025-10-27 16:03:15 +0530

    Added validation for missing email or password fields

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -70,7 +70,9 @@ const signup = async (req, res) => {
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-
+  if(!email || !password){
+    return res.status(400).json({message: "Email and password fields are required"});
+  }
   try {
     const user = await User.findOne({ email });
     if (!user) {
